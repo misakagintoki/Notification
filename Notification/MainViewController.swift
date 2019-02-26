a5b65ab110e306fabb2aa340fb8d554ebbb6a12d
USER <user@USERnoMacBook-Pro.local>
2019-02-26 15:09:33 +0900

    析构函数，自动调解高度

--- Notification/MainViewController.swift
+++ Notification/MainViewController.swift
@@ -9,14 +9,22 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
     @IBOutlet weak var label: UILabel!
     
     @IBOutlet weak var inputTableView: UITableView!
     
+    var array = ["hdfhadhfkjhjkfhkahkfhkaldlkfhfaksdfhkadfklldfkjladsjfjasdl;fjlamfl;nvnkjdfo;jldflsjdfl;dl",
+                 "aklsndfklngknkgnkajflkaldnrfkwenf;lknl;fdnlajelrjelfm;dmf;lmdl;f",
+                 "hdfhadhfkjhjkfhkahkfhkaldlkfhfaksdfhkadfklldfkjladsjfjasdl;fjlamfl;nvnkjdfo;jldflsjdfl;dl",
+                 "hdfhadhfkjhjkfhkahkfhkaldlkfhfaksdfhkadfklldfkjladsjfjasdl;fjlamfl;nvnkjdfo;jldflsjdfl;dl",
+                 "hdfhadhfkjhjkfhkahkfhkaldlkfhfaksdfhkadfklldfkjladsjfjasdl;fjlamfl;nvnkjdfo;jldflsjdfl;dl",
+                 "hdfhadhfkjhjkfhkahkfhkaldlkfhfaksdfhkadfklldfkjladsjfjasdl;fjlamfl;nvnkjdfo;jldflsjdfl;dl"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         label.text = "请继续输入"
         
         inputTableView.delegate = self
@@ -34,25 +42,39 @@ class MainViewController: UIViewController {
     @objc func changeLabel(){
         label.text = "不能再输入了。"
     }
-
-
+    
+    
 }
 
 extension MainViewController:UITableViewDelegate,UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 1 + array.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "InputCell", for: indexPath) as! inputTextViewCell
-//        cell.textView.delegate = self
-        return cell
+        if indexPath.row == 0{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "InputCell", for: indexPath) as! inputTextViewCell
+            //        cell.textView.delegate = self
+            return cell
+        }else{
+            let cell = UITableViewCell()
+            cell.textLabel?.text = "\(array[indexPath.row - 1])"
+            cell.textLabel?.numberOfLines = 0
+            return cell
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 10
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
+        if indexPath.row == 0{
+            return 80
+        }else{
+            return UITableView.automaticDimension//根据输入框的文字，自动调整cell的高度
+        }
     }
-    
 }
 //
 //extension inputTextViewCell:UITextViewDelegate{
